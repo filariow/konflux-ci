b089f61aeabbe927eeb47c80d6be18e1cbd3ca23
Francesco Ilario <filario@redhat.com>
2025-01-20 19:34:03 +0100

    add kyverno and generate kubearchiveconfig policy
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- deploy-deps.sh
+++ deploy-deps.sh
@@ -42,6 +42,8 @@ deploy() {
     deploy_cert_manager
     deploy_trust_manager
     kubectl apply -k "${script_path}/dependencies/cluster-issuer"
+    kubectl create -k "${script_path}/dependencies/kyverno"
+    kubectl create -k "${script_path}/dependencies/kubearchive"
     deploy_tekton
     deploy_dex
     deploy_registry
